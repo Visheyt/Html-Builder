a50b97a767f48c04615ca677c39953dfcf723dc7
Alex <fornikaras7@gmail.com>
2024-01-18 17:24:17 +0500

    feat: add simple code in  03-file-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+
+let directory = process.argv[1];
+let finaDirectory = path.join(directory, 'secret-folder');
+fs.readdir(finaDirectory, (err, items) => {
+  if (err) {
+    return err;
+  }
+  for (let item of items) {
+    fs.stat(path.join(finaDirectory, item), (err, stats) => {
+      if (err) {
+        return err;
+      }
+      if (stats.isFile()) {
+        let fileName = item.split('.');
+        console.log(
+          `${fileName[0]} - ${fileName[1]} - ${stats.size / 102400}mb`,
+        );
+      }
+    });
+  }
+});

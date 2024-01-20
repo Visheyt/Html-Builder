2c7d6c3a099f6aef0cd09b7e670ad144a9cfe846
Alex <fornikaras7@gmail.com>
2024-01-20 13:57:28 +0500

    feat: finish 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs/promises');
+const path = require('path');
+const fs2 = require('fs');
+
+async function mergeCss() {
+  let directory = path.join(__dirname, 'styles');
+  let finalDirectory = path.join(__dirname, 'project-dist');
+  try {
+    let cssFiles = await fs.readdir(directory, {
+      withFileTypes: true,
+    });
+    for (let cssFile of cssFiles) {
+      if (cssFile.name.split('.')[1] === 'css') {
+        let readStream = fs2.createReadStream(
+          path.join(directory, cssFile.name),
+          { encoding: 'utf-8' },
+        );
+        readStream.on('data', (chunk) => {
+          fs.appendFile(
+            path.join(finalDirectory, 'bundle.css'),
+            chunk,
+            (err) => {
+              if (err) throw err;
+            },
+          );
+        });
+      }
+    }
+  } catch (error) {
+    console.log(`We can\`t merge styles:${error}`);
+  }
+}
+mergeCss();
